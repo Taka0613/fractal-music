04391df4ca8b898000b535b9fa3733eb0f153c65
Adrian Badilla <badilladrianch@gmail.com>
2024-05-23 14:57:53 +0200

    chore: init develop branch & code standards

--- fractal/scale_maker.py
+++ fractal/scale_maker.py
@@ -1,28 +1,9 @@
+from constants import NOTE_INDEXES, SHARP_FLAT_EQUALITY_LIST
+
+
 def function(NOTE_INDEXES, PATTERN_RED, STARTING_NOTE):
     # define a sharp flat equality dictionary
-    SHARP_FLAT_EQUALITY_LIST = {
-    "C": 0,
-    "C#": 1,
-    "Db": 1,
-    "D": 2,
-    "D#": 3,
-    "Eb": 3,
-    "E": 4,
-    "Fb": 4,
-    "E#": 5,
-    "F": 5,
-    "F#": 6,
-    "Gb": 6,
-    "G": 7,
-    "G#": 8,
-    "Ab": 8,
-    "A": 9,
-    "A#": 10,
-    "Bb": 10,
-    "B": 11,
-    "Cb": 11,
-    "B#": 0
-    }
+    
     # return a sequence of notes
     generated_notes = []
     # keep track of the current index at each step
@@ -58,20 +39,7 @@ def function(NOTE_INDEXES, PATTERN_RED, STARTING_NOTE):
     print(generated_notes)
 
 #test case 1 
-NOTE_INDEXES = {
-    0: "C",
-    1: "C#",
-    2: "D",
-    3: "D#",
-    4: "E",
-    5: "F",
-    6: "F#",
-    7: "G",
-    8: "G#",
-    9: "A",
-    10: "A#",
-    11: "B"
-}
+
 PATTERN_RED =[2, 1, 2, 2, 1, 2, 2]
 STARTING_NOTE = "A"
 
