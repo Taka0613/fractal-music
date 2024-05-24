497b10c162254e14b8f532f58e0a7bc27428340e
bado <bado@bados-MacBook-Pro.local>
2024-05-24 14:53:58 +0200

    revert: delete main script and renaming to entry point
    reverting: main to original
    
    another change

--- main.py
+++ main.py
@@ -4,7 +4,3 @@ from fractal.scale_maker import generate_scale
 
 if __name__ == "__main__":
     a_scale_pattern_red = generate_scale(PATTERN_RED, "A")
-    print(a_scale_pattern_red) # ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'A']
-
-    d_scale_pattern_red = generate_scale(PATTERN_RED, "D")
-    print(d_scale_pattern_red) # ['D', 'E', 'F', 'G', 'A', 'Bb', 'C', 'D']
